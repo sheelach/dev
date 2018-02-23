1d63fdc72e0aab6b6af92aa1fe2b5b9aeed95adc
sheelach <sheelach@users.noreply.github.com>
2018-02-23 10:33:27 -0800

    Add files via upload
    factorial file commit

---
+++ factorial.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class factorial {
+	public static int factorial(int num)
+	{
+		if (num ==1)
+		{
+			return 1;
+		}
+		else
+		{
+			return num * factorial(num -1);
+		}
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Scanner sc = new Scanner(System.in);
+		 
+		 double sum = 0.0;
+		 for(int n=1;n<=10;n++)
+		 {
+		 int t = factorial(n);
+		 System.out.println(t);
+	}
+	}
+
+}
